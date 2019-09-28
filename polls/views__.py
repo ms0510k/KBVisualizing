e53af45b586e0f72b727f3e07392d3dc5b44cf00
minsungkim <ms0510k@gmail.com>
2019-09-28 19:59:23 +0900

    link dash, link click two step

--- polls/views__.py
+++ polls/views__.py
@@ -9,7 +9,7 @@ import json
 import os
 from DjangoTest.settings import BASE_DIR
 
-#engineDataPath = os.path.join(BASE_DIR, "polls/static/engineData/model_answer.csv")
+# 모델을 통해 추론된 트리플과 score 데이터 경로
 engineDataPath = os.path.join(BASE_DIR, "polls/static/engineData/demo.csv")
 data = pd.read_csv(engineDataPath, names=['e1','r','e2','score'])
 
@@ -23,10 +23,8 @@ targetPropertyTriplesRandom = []
 targetProperty = None
 json_l = []
 
-#dataPath = 'polls/static/data/entryKB.nt'
-#dataPath = 'polls/static/data/without_label_459.nt'
+# 추론 전 트리플 데이터 경
 dataPath = 'polls/static/data/demo.nt'
-#dataPath = 'polls/static/data/wiseKB_sample.nt'
 dataCount = 5000
 
 def ontologyDemo(request):
@@ -43,11 +41,11 @@ def ontologyDemo(request):
 
 g = rdflib.Graph()
 
+
 def readData(input_file):
     global l_org, l
     i = 0
     for line in input_file:
-        #print(line)
         s, p, o = line.replace('\n', '').replace("concept:", '').replace("concept_", '').split('\t')
         s = s.replace(s.split('_')[0] + '_', '')
         o = o.replace(o.split('_')[0] + '_', '')
@@ -55,8 +53,6 @@ def readData(input_file):
             i += 1
             l_org.append([s, p, o])
             l.append([s, p, o])
-            # if i <= 200:
-            #     l.append([s, p, o])
 
 def ntDraw(data_l):
     nodes = dict()
@@ -116,7 +112,8 @@ def ntDraw(data_l):
 
     myoutput.close()
 
-def graph(request):  ##### graph #####
+# Data를 읽어오는 함수
+def graph(request):
     global l_org, l
     if request.method == 'POST':
         content = True
@@ -126,7 +123,6 @@ def graph(request):  ##### graph #####
             if '.nt' in filePath:
                 input_file = open(filePath, 'r')
                 readData(input_file)
-                #ntDraw(l_org)
                 if len(l_org) > dataCount:
                     ntDraw(random.sample(l_org, dataCount))
                 else:
@@ -211,99 +207,11 @@ def graph(request):  ##### graph #####
 
         return HttpResponse(content, content_type='text/html')
 
-def exp(request):  ##### exp #####
-    if request.method == 'POST':
-        print("exp")
 
-
-        return HttpResponse("exp", content_type='text/html')
-
-def get_depth(depth, kb_df, target_relation):
-    # depth 0 : only target relation triples
-    prev_df = kb_df[kb_df.p.str.match(target_relation)]
-    prev_depth_node = []
-    # depth 1 ~~~~
-    for i in range(depth):
-        depth_node = prev_depth_node + list(prev_df.s.values) + list(prev_df.o.values)
-        depth_index = []
-
-        for row, col in kb_df.iterrows():
-            if (col['s'] in depth_node or col['o'] in depth_node ) :
-                depth_index.append(row)
-
-        # get triples
-        depth_df = kb_df.iloc[depth_index]
-
-        #update for next step
-        prev_depth_node = depth_node
-        prev_df = depth_df
-
-    return prev_df.values.tolist()
-
-def depth(request):  ##### depth select #####
-    global l, l_org, targetProperty
-    if request.method == 'POST':
-        depth = int(request.POST.get('n')) #depth 숫자 받기
-        #data = pd.read_csv(dataPath, sep='\t', names=['s', 'p', 'o'])
-        data = []
-        for line in open(dataPath, 'r'):
-            s, p, o = line.replace('\n', '').replace("concept:", '').replace("concept_", '').split('\t')
-            data.append([s, p, o])
-        data = pd.DataFrame(data)
-        data.columns = ['s', 'p', 'o']
-
-        #print(data[0:1])
-        for i in range(len(data)):
-            data['s'][i] = data['s'][i].replace(data['s'][i].split('_')[0] + '_', '')
-            data['o'][i] = data['o'][i].replace(data['o'][i].split('_')[0] + '_', '')
-
-
-        l = get_depth(depth, data, targetProperty)
-        ntDraw(l)
-        result = [depth,len(l)]
-
-        return HttpResponse(json.dumps(result), content_type='application/json')
-
-def targetProperty(request):  ##### targetProperty select #####
-    global l_org, l, targetPropertyTriples, targetPropertyTriplesRandom, targetProperty
-    if request.method == 'POST':
-        targetProperty = request.POST.get('targetProperty')
-        del targetPropertyTriples[:]
-        del targetPropertyTriplesRandom[:]
-
-        for i in range(len(l_org)):
-            if targetProperty == l_org[i][1]:
-                targetPropertyTriples.append(l_org[i])
-                flag = True
-
-        l = targetPropertyTriples
-
-        ntDraw(l)
-        return HttpResponse(json.dumps(l), content_type='text/html')
-
-def delTriple(request):  ##### Triple Delete & reDraw #####
-    global l_org, l, delSubject, delProperty, delObject
-    if request.method == 'POST':
-        delSubject = request.POST.get('subject')
-        delProperty = request.POST.get('property')
-        delObject = request.POST.get('object')
-        flag = False
-
-        for i in range(len(l)):
-            if l[i][0] == delSubject and l[i][2] == delObject:
-                flag = True
-                del l[i]
-                break
-        if flag:
-            ntDraw(l)
-        return HttpResponse(flag, content_type='text/html')
-
-def runEngine(request):  #####  #####
+def runEngine(request):
     global l_org, l, data, delSubject, delProperty, delObject
     if request.method == 'POST':
         runData = data[data['e1'].str.contains(delSubject) & data['e2'].str.contains(delObject)].sort_values('score', ascending=False)
-        #print("=========")
-        #print(runData)
         lst = []
         for i in range(len(runData)):
             tmp = []
@@ -322,89 +230,8 @@ def runEngine(request):  #####  #####
 
         return HttpResponse(json.dumps(lst), content_type='application/json')
 
-def objectReasoning(request):  ##### nell #####
-    global l_org, l, data, delSubject, delProperty, delObject
-    if request.method == 'POST':
-        object = request.POST.get('object')
-        threshold = request.POST.get('threshold')
-        threshold = float(threshold)
-        print(type(threshold),threshold)
-        runData = data[data['e2'].str.contains(object)].sort_values('score', ascending=False)
-        lst = []
-        json_l = []
-        tmp_l = copy.deepcopy(l)
-        l = []
-        for i in range(len(tmp_l)):
-            if tmp_l[i][2] != object:
-                l.append(tmp_l[i])
-        for i in range(len(runData)):
-            tmp = []
-            score = runData.values[i][-1]
-            e1 = runData.values[i][0]
-            r = runData.values[i][1]
-            e2 = runData.values[i][2]
-            tmp.append(e1)
-            tmp.append(r)
-            tmp.append(e2)
-            tmp.append(score)
-            lst.append(tmp)
-
-        # print(lst)
-        for i in range(len(lst)):
-            if lst[i][3] > threshold:
-                l.append([lst[i][0].replace(lst[i][0].split('_')[0] + '_', ''), lst[i][1].replace('concept:', ''),
-                          lst[i][2].replace(lst[i][2].split('_')[0] + '_', '')])
-                json_l.append([lst[i][0].replace(lst[i][0].split('_')[0] + '_', ''), lst[i][1].replace('concept:', ''),
-                               lst[i][2].replace(lst[i][2].split('_')[0] + '_', ''), lst[i][3]])
-        print("=======")
-        print(len(l))
-        print(l)
-        ntDraw(l)
-
-        return HttpResponse(json.dumps(json_l), content_type='application/json')
-
-def relationReasoning(request):  ##### nell #####
-    global l_org, l, data, delSubject, delProperty, delObject, json_l
-    if request.method == 'POST':
-        relation = request.POST.get('relation')
-        threshold = request.POST.get('threshold')
-        threshold = float(threshold)
-        print(type(threshold),threshold)
-        runData = data[data['r'].str.contains(relation)].sort_values('score', ascending=False)
-        lst = []
-        result_l = []
-        for i in range(len(runData)):
-            tmp = []
-            score = runData.values[i][-1]
-            e1 = runData.values[i][0]
-            r = runData.values[i][1]
-            e2 = runData.values[i][2]
-            tmp.append(e1)
-            tmp.append(r)
-            tmp.append(e2)
-            tmp.append(score)
-            lst.append(tmp)
-
-        print('lstlen:',len(lst))
-        print(lst)
-        #l = []
-        print('llen:',len(l))
-        for i in range(len(lst)):
-            if lst[i][3] > threshold:
-                l.append([lst[i][0].replace(lst[i][0].split('_')[0] + '_', ''), lst[i][1].replace('concept:', ''),
-                          lst[i][2].replace(lst[i][2].split('_')[0] + '_', '')])
-                json_l.append([lst[i][0].replace(lst[i][0].split('_')[0] + '_', ''), lst[i][1].replace('concept:', ''),
-                               lst[i][2].replace(lst[i][2].split('_')[0] + '_', '')])
-                result_l.append([lst[i][0].replace(lst[i][0].split('_')[0] + '_', ''), lst[i][1].replace('concept:', ''),
-                                 lst[i][2].replace(lst[i][2].split('_')[0] + '_', ''), lst[i][3]])
-        print("=======")
-        print(len(l))
-        print(l)
-        ntDraw(l)
-
-        return HttpResponse(json.dumps(result_l), content_type='application/json')
-
-def movieReasoning(request):  ##### nell #####
+# 모델을 통하여 추론된 데이터 전처리
+def movieReasoning(request):
     global l_org, l, data, delSubject, delProperty, delObject, json_l
     if request.method == 'POST':
         relation = request.POST.get('relation')
@@ -441,7 +268,7 @@ def movieReasoning(request):  ##### nell #####
 
         return HttpResponse(json.dumps(withScore), content_type='application/json')
 
-def inferredOnly(request):  #####  #####
+def inferredOnly(request):
     global l_org, l, data, delSubject, delProperty, delObject, json_l
     if request.method == 'POST':
         count = request.POST.get('count')
